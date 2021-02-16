244b9a44dd431c3d44894a0b5ea0236b48eb04a8
Said <carlos.said21@gmail.com>
2021-02-16 13:14:52 -0600

    Added dark mode to the web pages needed

--- scripts/darkmode.js
+++ scripts/darkmode.js
@@ -1,27 +1,71 @@
 let darkMode = localStorage.getItem('darkMode1');
 const darkModeToggle = document.querySelector(".darkModeToggle");
-const selectGroup = document.getElementById("SelectGroup");
 
 function enableDarkMode() {
     document.body.classList.add("dark");
-    const members = document.querySelectorAll(".member");
-    members.forEach((member) => {
-        console.log(member);
-        member.classList.add("dark");
-        console.log(member);
-    });
-    selectGroup.classList.add("dark");
+    try {//try for members.html
+        const selectGroup = document.getElementById("SelectGroup");
+        const members = document.querySelectorAll(".member");
+        members.forEach((member) => {
+            console.log(member);
+            member.classList.add("dark");
+            console.log(member);
+        });
+        selectGroup.classList.add("dark");
+    } catch (error) { }
+    try {//try for dashboard.html
+        const links = document.querySelector(".links");
+        const viewMain = document.querySelector(".view_main");
+        const viewItiem = document.querySelectorAll(".view_item");
+        viewItiem.forEach((item) => {
+            console.log(item);
+            item.classList.add("dark");
+            console.log(item);
+        });
+        links.classList.add("dark");
+        viewMain.classList.add("dark");
+    } catch (error) { }
+
+    try {//try for notes.js
+        const todoList = document.querySelector(".todo-list");
+        todoList.classList.add("dark");
+    } catch (error) { }
+
 }
 
 function diasbleDarkMode() {
     document.body.classList.remove("dark");
-    const members = document.querySelectorAll(".member");
-    members.forEach((member) => {
-        console.log(member);
-        member.classList.remove("dark");
-        console.log(member);
-    });
-    selectGroup.classList.remove("dark");
+    try {//try for members.html
+        const members = document.querySelectorAll(".member");
+        members.forEach((member) => {
+            console.log(member);
+            member.classList.remove("dark");
+            console.log(member);
+        });
+        selectGroup.classList.remove("dark");
+
+    } catch (error) { }
+
+    try {//try for dashboard.html
+        const links = document.querySelector(".links");
+        const viewMain = document.querySelector(".view_main");
+        const viewItiem = document.querySelectorAll(".view_item");
+        viewItiem.forEach((item) => {
+            console.log(item);
+            item.classList.remove("dark");
+            console.log(item);
+        });
+        links.classList.remove("dark");
+        viewMain.classList.remove("dark");
+
+
+    } catch (error) { }
+
+    try {//try for notes.js
+        const todoList = document.querySelector(".todo-list");
+        todoList.classList.remove("dark");
+    } catch (error) { }
+
 }
 console.log("Recien abierto" + darkMode);
 
